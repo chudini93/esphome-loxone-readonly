c24c958e887da180725cf5a3f3b3177926844265
chudini93 <bartlomiejmarek93@gmail.com>
2025-04-13 03:15:19 +0200

    Update udp_esp8266_component.cpp

--- components/udp_esp8266/udp_esp8266_component.cpp
+++ components/udp_esp8266/udp_esp8266_component.cpp
@@ -5,43 +5,15 @@ namespace esphome {
 
     void ESP8266UdpComponent::setup() {}
 
-    void ESP8266UdpComponent::update() {
-      if (!network::is_connected()) {
-        ESP_LOGD(TAG, "Network not ready");
-        return;
-      }
-      
-      // Handle outgoing buffer
-      while (!send_string_buffer_.empty()) {
-        udp_.beginPacket(loxone_ip_.c_str(), loxone_port_);
-        udp_.write(send_string_buffer_.front().c_str());
-        udp_.write(delimiter_.c_str());
-        udp_.endPacket();
+    void ESP8266UdpComponent::update() {}
 
-        ESP_LOGD(TAG, "Sent string data: %s", send_string_buffer_.front().c_str());
-        send_string_buffer_.pop();
-      }
-    }
-
-    bool disableUdpBuffer = true;
     void ESP8266UdpComponent::send_string_data(std::string data) {
-      if (disableUdpBuffer){
         udp_.beginPacket(loxone_ip_.c_str(), loxone_port_);
         udp_.write(data.c_str());
         udp_.write(delimiter_.c_str());
         udp_.endPacket();
         
-        ESP_LOGD(TAG, "Sent string data: %s", data.c_str());
-        return;
-      }
-      
-      if (send_string_buffer_.size() >= send_buffer_length_) {
-        ESP_LOGW(TAG, "Send buffer full. Dropping oldest data.");
-        send_string_buffer_.pop();
-      }
-
-      send_string_buffer_.push(data);
-      ESP_LOGD(TAG, "Queued string data for sending: %s", data.c_str());
+        ESP_LOGD(TAG, "UDP message sent: %s", data.c_str());
     }
 
   }  // namespace udp_esp8266
