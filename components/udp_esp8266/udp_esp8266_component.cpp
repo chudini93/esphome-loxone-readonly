da79c0b8aabf01c21ae358aec9085daa1531c936
chudini93 <bartlomiejmarek93@gmail.com>
2025-04-13 03:09:03 +0200

    Update udp_esp8266_component.cpp

--- components/udp_esp8266/udp_esp8266_component.cpp
+++ components/udp_esp8266/udp_esp8266_component.cpp
@@ -23,7 +23,18 @@ namespace esphome {
       }
     }
 
+    bool disableUdpBuffer = true;
     void ESP8266UdpComponent::send_string_data(std::string data) {
+      if (disableUdpBuffer){
+        udp_.beginPacket(loxone_ip_.c_str(), loxone_port_);
+        udp_.write(data);
+        udp_.write(delimiter_.c_str());
+        udp_.endPacket();
+        
+        ESP_LOGD(TAG, "Sent string data: %s", data);
+        return;
+      }
+      
       if (send_string_buffer_.size() >= send_buffer_length_) {
         ESP_LOGW(TAG, "Send buffer full. Dropping oldest data.");
         send_string_buffer_.pop();
